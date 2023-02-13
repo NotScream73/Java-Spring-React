b77b39e859168590c7440c7d32998d5b3a65b651
Katerina881 <mr.alodov@mail.ru>
2023-02-13 17:27:49 +0400

    Первая лаба

---
+++ Front/script.js
@@ -0,0 +1,37 @@
+let calculateButton = document.getElementById("calculate");
+let numberOneInput = document.getElementById("first");
+let numberTwoInput = document.getElementById("second");
+let resultInput = document.getElementById("res");
+
+buttonPlus.onclick = function(event) {
+    event.preventDefault();
+    let num_1 = numberOneInput.value;
+    let num_2 = numberTwoInput.value;
+    fetch(`http://localhost:8080/sum?first=${num_1}&second=${num_2}`)
+        .then(response => response.text())
+        .then(res => resultInput.value = res);
+}
+buttonMinus.onclick = function(event) {
+   event.preventDefault();
+   let num_1 = numberOneInput.value;
+   let num_2 = numberTwoInput.value;
+   fetch(`http://localhost:8080/minus?first=${num_1}&second=${num_2}`)
+       .then(response => response.text())
+       .then(res => resultInput.value = res);
+}
+buttonMulti.onclick = function(event) {
+   event.preventDefault();
+   let num_1 = numberOneInput.value;
+   let num_2 = numberTwoInput.value;
+   fetch(`http://localhost:8080/multi?first=${num_1}&second=${num_2}`)
+       .then(response => response.text())
+       .then(res => resultInput.value = res);
+}
+buttonDiv.onclick = function(event) {
+     event.preventDefault();
+     let num_1 = numberOneInput.value;
+     let num_2 = numberTwoInput.value;
+     fetch(`http://localhost:8080/div?first=${num_1}&second=${num_2}`)
+         .then(response => response.text())
+         .then(res => resultInput.value = res);
+ }
--- src/main/java/ip/labwork/LabworkApplication.java
+++ src/main/java/ip/labwork/LabworkApplication.java
@@ -12,8 +12,36 @@ public class LabworkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LabworkApplication.class, args);
 	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/sum")
+	public String Sum(@RequestParam(defaultValue = "0") double first,
+					  @RequestParam(defaultValue = "0") double second) {
+		return Double.toString(first + second);
+	}
+
+	@GetMapping("/minus")
+	public String Ras(@RequestParam(defaultValue = "0") double first,
+					  @RequestParam(defaultValue = "0") double second) {
+		return Double.toString(first - second);
+	}
+
+	@GetMapping("/multi")
+	public String Pros(@RequestParam(defaultValue = "1") double first,
+					   @RequestParam(defaultValue = "1") double second) {
+		return Double.toString(first * second);
+	}
+
+	@GetMapping("/div")
+	public String Del(@RequestParam(defaultValue = "1") double first,
+					  @RequestParam(defaultValue = "1") double second) {
+		if (second == 0) {
+			return null;
+		}
+		return Double.toString(first / second);
 	}
 }
